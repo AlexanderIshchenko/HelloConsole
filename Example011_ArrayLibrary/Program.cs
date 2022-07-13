ce509060696a2dd9d5263b206cbce324437e9109
Alexander <063@mail.ru>
2022-07-13 23:50:09 +0400

    Ex_002,008-011

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,56 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index ++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // элемент не найден. Если числа find нет в массиве.
+                        // Если оставить 0, то будет выводить 0, что означает
+                        // 0-ой элемент массива. Это ошибка!
+
+    while(index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int [10]; // создай новый массив из 10 элементов.
+                            // по началу он будет наполнен нулями
+
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 434);
+Console.WriteLine(pos);
+
